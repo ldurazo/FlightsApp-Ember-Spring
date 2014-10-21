d82800b6f0ec21c21c82254c219075fad04762a0
ldurazo <luisdurazoa@gmail.com>
2014-10-20 20:23:25 -0700

    Can now model the search as a json string.

--- client-side-ember/js/app.js
+++ client-side-ember/js/app.js
@@ -1,49 +1,65 @@
-App = Ember.Application.create();
-
-App.Router.map(function() {
+App = Ember.Application.create({
+    LOG_ACTIVE_GENERATION: true,
+    LOG_TRANSITIONS: true,
+    LOG_TRANSITIONS_INTERNAL: false,
+    LOG_VIEW_LOOKUPS: true
 });
 
-App.IndexRoute = Ember.Route.extend({
+App.Router.map(function() {
+    this.route("reservations");
+    this.resource('flights', { path: '/flights'});
+});
+
+App.FlightsRoute = Ember.Route.extend({
     model: function() {
-        Uncomment this, this is the model that should be retrieved
-        return this.store.find('Flight');
+//        Uncomment this, this is the model that should be retrieved
+//        return this.store.find("flight");
+
+        //this contains flights key on json to consume
+//        return $.getJSON("http://www.mocky.io/v2/5445a3371db7968c1c5f24be", function(data) {
+//                                              return data;
+//                                          });
+
+        //This is without the flights key on json to consume
+      return $.getJSON("http://www.mocky.io/v2/542f03d5f190d8220c26e891", function(data) {
+                                            return data;
+                                        });
+
     }
 });
 
-App.IndexController = Ember.ArrayController.extend({
-    needs: "application"
-});
+App.FlightsController = Ember.ArrayController.extend({});
 
-App.IndexAdapter = DS.RESTAdapter.extend({
-  host: 'http://www.mocky.io/v2/542f03d5f190d8220c26e891'
+App.ApplicationAdapter = DS.RESTAdapter.extend({
+    host:"http://www.mocky.io/v2/5445a3371db7968c1c5f24be"
 });
 
 App.ApplicationController = Ember.ObjectController.extend({
-  passengerslimit: [1,2,3,4,5,6],
-  departureCity:null,
-  arrivalCity:null,
-  departureDate:null,
-  returnDate:null,
-  passengersNumber:null,
-  actions: {
-      searchFlights: function(){
-        var departureCity = this.get('departureCity');
-        var departureDate = this.get('departureDate');
-        var arrivalCity = this.get('arrivalCity');
-        var returnDate = this.get('returnDate');
-        var passengersNumber = this.get('passengersNumber');
-        console.log(departureDate +" "+ departureCity +" "+ arrivalCity +" "+ returnDate +" "+ passengersNumber);
+      passengerslimit: [1,2,3,4,5,6],
+      departureCity:null,
+      arrivalCity:null,
+      departureDate:null,
+      returnDate:null,
+      passengersNumber:null,
+      actions: {
+          searchFlights: function(){
+            var departureCity = this.get('departureCity');
+            var departureDate = this.get('departureDate');
+            var arrivalCity = this.get('arrivalCity');
+            var returnDate = this.get('returnDate');
+            var passengersNumber = this.get('passengersNumber');
+            var searchJson = JSON.stringify({
+            departureCity: departureCity,
+                            arrivalCity: arrivalCity,
+                            departureDate: departureDate,
+                            returnDate: returnDate,
+                            passengersNumber: passengersNumber
+            });
+            console.log(searchJson);
+            this.transitionToRoute('flights');
+          }
       }
-  }
 });
-
-App.PostRequest=DS.Model.extend({
-    departureCity: DS.attr('string'),
-    arrivalCity: DS.attr('string'),
-    returnDate: DS.attr('string'),
-    departureDate: DS.attr('string')
-    passengersNumber: DS.attr('string')
-})
 
 App.Flight = DS.Model.extend({
     saleTotal: DS.attr('string'),
@@ -59,8 +75,8 @@ App.Segment = DS.Model.extend({
 });
 
 App.Leg = DS.Model.extend({
-    arrivalTime: DS.attr('string'),
-    departureTime: DS.attr('string'),
+    arrivalTime: DS.attr('date'),
+    departureTime: DS.attr('date'),
     origin: DS.attr('string'),
     destination: DS.attr('string')
 });
